8dc07d7a0a0b3064dcf3396d9cbbc70893bfa948
Diego <diegortizte@gmail.com>
2024-02-02 01:01:14 -0300

    feat: navbar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ServicesComponent } from './components/pages/services/services.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'servicios', component: ServicesComponent}
 ];

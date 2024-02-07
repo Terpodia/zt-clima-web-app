633e5699fee0a38842a1a6381a3642a779d4d686
Juan <juan@dd3tech.mx>
2024-02-07 06:41:14 -0600

    feat: preview page

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -18,12 +18,12 @@ export class GalleryComponent {
   currentIndex: number = 0;
 
   galleryImages: string[] = [
-    '../../../assets/images/1.jpg',
-    '../../../assets/images/2.jpg',
-    '../../../assets/images/3.jpg',
-    '../../../assets/images/4.jpg',
-    '../../../assets/images/5.jpg',
-    '../../../assets/images/6.jpg',
+    'assets/images/1.jpg',
+    'assets/images/2.jpg',
+    'assets/images/3.jpg',
+    'assets/images/4.jpg',
+    'assets/images/5.jpg',
+    'assets/images/6.jpg',
   ];
 
   prev(): void {

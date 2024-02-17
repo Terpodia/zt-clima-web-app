d21fe9ab8fdbbb20910b18d3a671754fd099183a
Juan <juan@dd3tech.mx>
2024-02-17 08:52:03 -0600

    feat: whatsapp button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+const WHATSAPP_API =
+  'https://api.whatsapp.com/send/?type=phone_number&app_absent=0';
+const phone = '5491161818130';
+const msg = 'Hola ZT Clima, necesito asesoramiento.';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,4 +17,5 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class AppComponent {
   title = 'zt-clima-web-app';
+  whatsaaUrl = WHATSAPP_API + `&phone=${phone}&text=${msg}`;
 }

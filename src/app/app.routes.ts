08e16aa8fa4f8f8d6a2c96caf12906801ed4d324
Juan <juan@dd3tech.mx>
2024-03-20 17:16:21 -0600

    feat: spa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/pages/home/home.component';
-import { ServicesComponent } from './components/pages/services/services.component';
-import { ClientsComponent } from './components/pages/clients/clients.component';
-import { ProductsComponent } from './components/pages/products/products.component';
-import { ContactComponent } from './components/pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'servicios', component: ServicesComponent },
-  { path: 'clientes', component: ClientsComponent },
-  { path: 'productos', component: ProductsComponent },
-  { path: 'contacto', component: ContactComponent },
 ];

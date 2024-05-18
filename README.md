b3c43e18df990d764cd8a769309f1a5500474f27
Juan <juan@dd3tech.mx>
2024-05-18 16:10:59 -0600

    feat: services and products section 0.2

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
+For github:
+- ng build --base-href /zt-clima-web-app/
+- and move the dist/client folder to docs/
+
+And for a server just with ng build
+
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,44 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
-
+  images = [
+    {
+      src: 'assets/images/products/AristonLogo.png',
+      title: 'Ariston Logo',
+      description: 'Ariston Logo',
+    },
+    {
+      src: 'assets/images/products/Baxi Logo.png',
+      title: 'Baxi Logo',
+      description: 'Baxi Logo',
+    },
+    {
+      src: 'assets/images/products/Ferroli Logo.png',
+      title: 'Ferroli Logo',
+      description: 'Ferroli Logo',
+    },
+    {
+      src: 'assets/images/products/Giacomini Logo.png',
+      title: 'Giacomini Logo',
+      description: 'Giacomini Logo',
+    },
+    {
+      src: 'assets/images/products/Peisa Logo.png',
+      title: 'Peisa Logo',
+      description: 'Peisa Logo',
+    },
+    {
+      src: 'assets/images/products/Rehau Logo.png',
+      title: 'Rehau Logo',
+      description: 'Rehau Logo',
+    },
+  ];
 }

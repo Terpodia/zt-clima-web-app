b7c6bf40dc07d6cfe194ba71f08b0128feb7774d
Juan <juan@dd3tech.mx>
2024-05-27 11:34:02 -0400

    feat: added product description

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -12,33 +12,33 @@ export class ProductsComponent {
   images = [
     {
       src: 'assets/images/products/AristonLogo.png',
-      title: 'Ariston Logo',
-      description: 'Ariston Logo',
+      title: 'Ariston',
+      description: 'Ariston, es una marca italiana con más de 90 años de experiencia en el mercado global, ofrece productos innovadores y eficientes para calefacción y agua caliente, destacando por su calidad duradera, sostenibilidad y facilidad de uso.',
     },
     {
       src: 'assets/images/products/Baxi Logo.png',
-      title: 'Baxi Logo',
-      description: 'Baxi Logo',
+      title: 'Baxi',
+      description: 'Baxi, con más de 150 años de historia, proporciona productos de calefacción y agua caliente conocidos por su fiabilidad y durabilidad. La marca combina una larga tradición con tecnologías modernas para asegurar el confort y el ahorro energético. ',
     },
     {
       src: 'assets/images/products/Ferroli Logo.png',
-      title: 'Ferroli Logo',
-      description: 'Ferroli Logo',
+      title: 'Ferroli',
+      description: 'Ferroli, con más de 65 años en Italia, se especializa en productos para calefacción, los cuales son valorados por su robustez y capacidad para proporcionar un rendimiento constante, adaptándose a las necesidades de los usuarios. ',
     },
     {
       src: 'assets/images/products/Giacomini Logo.png',
-      title: 'Giacomini Logo',
-      description: 'Giacomini Logo',
+      title: 'Giacomini',
+      description: 'Giacomini, es una empresa italiana con más de 60 años en el mercado, especializándose en la fabricación de caños y accesorios para calefacción, destacando por su precisión, eficiencia y durabilidad.',
     },
     {
       src: 'assets/images/products/Peisa Logo.png',
-      title: 'Peisa Logo',
-      description: 'Peisa Logo',
+      title: 'Peisa',
+      description: 'Peisa, con más de 40 años de trayectoria en Argentina, es conocida por sus productos de climatización que combinan diseño avanzado y rendimiento fiable.',
     },
     {
       src: 'assets/images/products/Rehau Logo.png',
-      title: 'Rehau Logo',
-      description: 'Rehau Logo',
+      title: 'Rehau',
+      description: 'Rehau, de origen alemán y con más de 70 años en el mercado, es líder en productos de climatización y fontanería. Sus productos son conocidos por ofrecer altos estándares de calidad y sostenibilidad.',
     },
   ];
 }

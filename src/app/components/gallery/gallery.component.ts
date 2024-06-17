af1b7a301fea928f2cc619f68946afca2df99b72
Juan <juan@dd3tech.mx>
2024-06-17 16:59:40 -0400

    feat: obras realizadas section

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faChevronCircleLeft,
@@ -17,14 +17,7 @@ export class GalleryComponent {
   faChevronRight = faChevronCircleRight;
   currentIndex: number = 0;
 
-  galleryImages: string[] = [
-    'assets/images/1.jpg',
-    'assets/images/2.jpg',
-    'assets/images/3.jpg',
-    'assets/images/4.jpg',
-    'assets/images/5.jpg',
-    'assets/images/6.jpg',
-  ];
+  @Input() galleryImages!: string[];
 
   prev(): void {
     this.currentIndex =
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,4 +20,23 @@ import { ContactComponent } from '../../components/contact/contact.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  mainGallery = [
+    'assets/images/1.jpg',
+    'assets/images/2.jpg',
+    'assets/images/3.jpg',
+    'assets/images/4.jpg',
+    'assets/images/5.jpg',
+    'assets/images/6.jpg',
+  ];
+
+  obrasRealizadasGallery = [
+    'assets/images/obras/1.jpg',
+    'assets/images/obras/2.jpg',
+    'assets/images/obras/3.jpg',
+    'assets/images/obras/4.jpg',
+    'assets/images/obras/5.jpg',
+    'assets/images/obras/6.jpg',
+  ];
+}

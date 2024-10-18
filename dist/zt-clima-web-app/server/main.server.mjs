759cc01c9294e920656d238a8de11619f17dcfe4
ortiz.juan14 <ortiz.juan14@gmail.com>
2024-10-18 12:13:59 +0000

    fix: typo

--- dist/zt-clima-web-app/server/main.server.mjs
+++ dist/zt-clima-web-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RSJLFPUG.mjs";import"./chunk-YZDN7T24.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-J7OASOYZ.mjs";import"./chunk-YZDN7T24.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

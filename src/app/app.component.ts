3f22da209c10f582dbc2508c4b101791837295cb
ortiz.juan14 <ortiz.juan14@gmail.com>
2024-11-19 23:45:28 +0000

    feat: GTM events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 const WHATSAPP_API =
   'https://api.whatsapp.com/send/?type=phone_number&app_absent=0';
@@ -18,4 +19,16 @@ const msg = 'Hola ZT Clima, necesito asesoramiento.';
 export class AppComponent {
   title = 'zt-clima-web-app';
   whatsappUrl = WHATSAPP_API + `&phone=${phone}&text=${msg}`;
+
+  constructor(
+    private readonly gtmService: GoogleTagManagerService
+  ) {}
+
+  sendGTMEvent() {
+    const gtmTag = {
+      event: 'ChatJoin',
+    };
+
+    this.gtmService.pushTag(gtmTag);
+  }
 }

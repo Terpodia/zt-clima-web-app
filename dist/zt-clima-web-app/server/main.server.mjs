8ba42241622d847a47fcc5715a216cb566515b1f
ortiz.juan14 <ortiz.juan14@gmail.com>
2024-11-19 23:47:37 +0000

    feat: build GTM events

--- dist/zt-clima-web-app/server/main.server.mjs
+++ dist/zt-clima-web-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-J7OASOYZ.mjs";import"./chunk-YZDN7T24.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-TSF6DR7Q.mjs";import"./chunk-J4RJTO3K.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};

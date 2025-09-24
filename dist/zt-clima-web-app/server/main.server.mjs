36dc2b10687988d065f76e48d201d033a94bd77d
jota <ortiz.juan14@gmail.com>
2025-09-23 23:35:45 -0300

    chore: update main script hash and fix gallery height styling

--- dist/zt-clima-web-app/server/main.server.mjs
+++ dist/zt-clima-web-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-L6CGB36O.mjs";import"./chunk-NB5IRN6H.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BMUUXIKI.mjs";import"./chunk-NB5IRN6H.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
